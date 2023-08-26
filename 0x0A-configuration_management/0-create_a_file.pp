9fa0bbea4c3d799581862e366f117f2dd397b07c
BAISSANE <baissanee@gmail.com>
2023-08-25 20:05:14 -0700

    Updates

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-#create a file in /tmp.
-
-file { '/tmp/holberton':
-ensure  => 'present',
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I love Puppet',}
+# Creates a file
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
