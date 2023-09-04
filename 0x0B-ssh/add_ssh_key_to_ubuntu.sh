6a67644f70434fbd1eb6123ed9a3d877cc54ee59
BAISSANE <baissanee@gmail.com>
2023-09-04 07:07:49 -0700

    Updates

---
+++ 0x0B-ssh/add_ssh_key_to_ubuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Replace with your server's IP address
+SERVER_IP="54.82.196.192"
+
+# SSH public key to add
+SSH_PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDNdtrNGtTXe5Tp1EJQop8mOSAuRGLjJ6DW4PqX4wId/Kawz35ESampIqHSOTJmbQ8UlxdJuk0gAXKk3Ncle4safGYqM/VeDK3LN5iAJxf4kcaxNtS3eVxWBE5iF3FbIjOqwxw5Lf5sRa5yXxA8HfWidhbIG5TqKL922hPgsCGABIrXRlfZYeC0FEuPWdr6smOElSVvIXthRWp9cr685KdCI+COxlj1RdVsvIo+zunmLACF9PYdjB2s96Fn0ocD3c5SGLvDOFCyvDojSAOyE70ebIElnskKsDTGwfT4P6jh9OBzTyQEIS2jOaE5RQq4IB4DsMhvbjDSQrP0MdCLgwkN"
+
+# SSH into the server and add the SSH public key to authorized_keys
+ssh ubuntu@"$SERVER_IP" "mkdir -p ~/.ssh && echo '$SSH_PUBLIC_KEY' >> ~/.ssh/authorized_keys && chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys && sudo service ssh restart"
+
